41b11306a3a4220f6f5407ff8fae8b0dc73ca20f
WillianPaiva <vervalenpaiva@gmail.com>
2016-10-28 21:37:27 +0200

    some tweaks on the profile view

--- src/web/ScrumApp/resources/views/profile.blade.php
+++ src/web/ScrumApp/resources/views/profile.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<table class="table table-hover">
-<tr>
-  <td>Nom:</td>
-  <td>{{ Auth::user()->name }}</td>
-</tr>
-<tr>
-  <td>Email:</td>
-  <td> {{ Auth::user()->email }}</td>
-</tr>
-</table>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">User Profile</div>
+                <div class="panel-body">
+                    <table class="table table-hover">
+                    <tr>
+                        <td>Nom:</td>
+                        <td>{{ Auth::user()->name }}</td>
+                    </tr>
+                    <tr>
+                        <td>Email:</td>
+                        <td> {{ Auth::user()->email }}</td>
+                    </tr>
+                    </table>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
