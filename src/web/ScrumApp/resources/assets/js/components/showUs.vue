1c0f33baa7651c10e20d030702a30033e68b0420
WillianPaiva <vervalenpaiva@gmail.com>
2016-11-10 09:50:18 +0100

    adapt the userstory.vue

--- src/web/ScrumApp/resources/assets/js/components/showUs.vue
+++ src/web/ScrumApp/resources/assets/js/components/showUs.vue
@@ -3,7 +3,7 @@
             <div class="row">
                 <div class="col-md-12 col-md-offset-0">
                     <div class="panel panel-default">
-                        <div class="panel-heading">
+                        <div class="panel-heading clearfix">
                             <label class="label label-warning" style="margin-right: 7px;">
                                 US#{{nb}}
                             </label>
@@ -18,6 +18,10 @@
                                 <span class="badge">{{ us.priority }}</span> 
                             </label>
 
+                                 <button class="btn btn-success pull-right" v-on:click="showAddTask = true">
+                                     New Task
+                                 </button>
+
                         </div>
                         <div class="panel-body">
                             <label class="col-md-4 control-label" for="">Description</label>
@@ -27,11 +31,18 @@
                             <label class="col-md-4 control-label" for="">Tasks</label>
                         </br>
                         <div class="well" v-if="isEmpty()"><h3>no tasks</h3></div>
+
                         </div>
 
                     </div>
                 </div>
             </div>
+            <createtask
+                v-bind:showAddTask="showAddTask"
+                :id="usid"
+                @cancel="close"
+                @ok="close"
+            ></createtask>
     </div>
 </template>
 <script>
@@ -39,6 +50,8 @@
      data(){
          return{
              tasks:[],
+             usid:'',
+             showAddTask:false,
              us:{
              description: '',
              status: '',
@@ -71,6 +84,10 @@
          },
          isEmpty: function(){
              return !(this.tasks.length > 0)
+         },
+         close: function(){
+             this.showAddTask = false;
+             this.getUS();
          }
      }
 
