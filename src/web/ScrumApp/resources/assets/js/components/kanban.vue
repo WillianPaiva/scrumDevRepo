e876d5c1c823933573c381f4c78eacf84eae554c
WillianPaiva <vervalenpaiva@gmail.com>
2016-11-16 18:50:34 +0100

    create the kanban vue

---
+++ src/web/ScrumApp/resources/assets/js/components/kanban.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="DocumentList">
+            <sortablecoluns :sprintid="sprintid"></sortablecoluns>
+        </div>
+    </div>
+</template>
+<script>
+ export default{
+     props:['sprintid'],
+
+ }
+</script>
+<style>
+ .DocumentList
+{
+    /* overflow-x:hidden; */
+    /* overflow-y:hidden; */
+    height:100%;
+    width:100%;
+    padding: 0 15px;
+}
+
+.DocumentItem
+{
+    padding:0;
+    height:100%;
+    width:300px;
+}
+
+.list-inline {
+  white-space:nowrap;
+}
+
+</style>
