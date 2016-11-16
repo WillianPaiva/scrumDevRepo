1fc40c04f4686a1bf8d0f5bcbcffcf3c9c72382b
WillianPaiva <vervalenpaiva@gmail.com>
2016-11-16 18:49:22 +0100

    add sortablejs

--- src/web/ScrumApp/resources/assets/js/app.js
+++ src/web/ScrumApp/resources/assets/js/app.js
@@ -4,6 +4,7 @@
  * include Vue and Vue Resource. This gives a great starting point for
  * building robust, powerful web applications using Vue and Laravel.
  */
+import Sortable from 'sortablejs';
 
 require('./bootstrap');
 
@@ -23,6 +24,8 @@ Vue.component(
     'passport-personal-access-tokens',
     require('./components/passport/PersonalAccessTokens.vue')
 );
+
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
