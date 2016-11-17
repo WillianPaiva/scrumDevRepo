2196e0815f8cf34f3c806d2a7622b7d5f34562a5
WillianPaiva <vervalenpaiva@gmail.com>
2016-11-17 13:30:08 +0100

    register sortabletasks

--- src/web/ScrumApp/resources/assets/js/app.js
+++ src/web/ScrumApp/resources/assets/js/app.js
@@ -43,6 +43,7 @@ Vue.component('showus', require('./components/showUs.vue'));
 Vue.component('editus',require('./components/editus.vue'));
 Vue.component('editsprint',require('./components/editSprint.vue'));
 Vue.component('kanban',require('./components/kanban.vue'));
+Vue.component('sortabletasks',require('./components/sortabletasks.vue'));
 Vue.component('sortablecoluns',require('./components/sortablecoluns.vue'));
 
 // Vue.component('members', require('./components/Members.vue'));
