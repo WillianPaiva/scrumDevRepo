8957a132c03ba4dbade2d811ae980f5baa1cf6d1
WillianPaiva <vervalenpaiva@gmail.com>
2016-11-17 13:29:49 +0100

    fix style

--- src/web/ScrumApp/resources/assets/js/components/kanban.vue
+++ src/web/ScrumApp/resources/assets/js/components/kanban.vue
@@ -8,28 +8,27 @@
 <script>
  export default{
      props:['sprintid'],
-
  }
 </script>
 <style>
  .DocumentList
-{
-    /* overflow-x:hidden; */
-    /* overflow-y:hidden; */
-    height:100%;
-    width:100%;
-    padding: 0 15px;
-}
+ {
+     /* overflow-x:hidden; */
+     /* overflow-y:hidden; */
+     height:100%;
+     width:100%;
+     padding: 0 15px;
+ }
 
-.DocumentItem
-{
-    padding:0;
-    height:100%;
-    width:300px;
-}
+ .DocumentItem
+ {
+     padding:0;
+     height:100%;
+     width:300px;
+ }
 
-.list-inline {
-  white-space:nowrap;
-}
+ .list-inline {
+     white-space:nowrap;
+ }
 
 </style>
