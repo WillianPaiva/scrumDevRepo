05b95b7e7b48871bd5c4f032333d68747ca88f41
WillianPaiva <vervalenpaiva@gmail.com>
2016-12-01 20:44:59 +0100

    remove bug useless code

--- src/web/ScrumApp/routes/api.php
+++ src/web/ScrumApp/routes/api.php
@@ -449,24 +449,6 @@ Route::group(['middleware' => 'api'], function() {
 
 
 
-    //will be removed
-    Route::get('firstprojectusid/{id}',function($id){
-        $sprint = App\Sprint::find($id);
-        $project = App\Project::find($sprint->project_id);
-        $uslist=$project->UserStorys()->get();
-        foreach ($uslist as $us) {
-            foreach ($us->Tasks()->get() as $task) {
-                if(isset($result)){
-                    if($task->user_story_id < $result){
-                        $result = $task->user_story_id;
-                    }
-                }
-                else{
-                    $result = $task->user_story_id;
-                }    }
-        }
-        return Response::json($result);
-    });
 
 
 
